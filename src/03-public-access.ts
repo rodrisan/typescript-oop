e728e3514a9bdbac4a1eddc0a728088bb6a33730
Rodrigo Sarmiento <info@rodrigosarmiento.com>
2023-05-23 10:31:06 -0600

    feat(): working public access

---
+++ src/03-public-access.ts
@@ -0,0 +1,37 @@
+export class MyDate {
+  // public readonly year: number;
+  // Readonly affects internally and externally.
+  public year: number;
+  public month: number;
+  public day: number;
+
+  constructor(year: number, month: number, day: number) {
+    this.year = year;
+    this.month = month;
+    this.day = day;
+  }
+
+  public printFormat(): string {
+    return `${this.day}/${this.month}/${this.year}`;
+  }
+
+  public add(amount: number, type: 'days' | 'months' | 'years') {
+    if (type === 'days') {
+      this.day += amount;
+    }
+    if (type === 'months') {
+      this.month += amount;
+    }
+    if (type === 'years') {
+      this.year += amount;
+    }
+  }
+}
+
+const myDate = new MyDate(1993, 7, 1);
+console.log(myDate.year);
+myDate.year = 1994;
+console.log(myDate.year);
+console.log(myDate.month);
+console.log(myDate.day);
+
