694f39fa35390c01e55f6129e3a133ebf5c9d4bd
Rodrigo Sarmiento <rodrigo.sarmiento@yaydoo.com>
2023-05-23 12:37:37 -0600

    feat(): working with singletons part2

--- src/13-singleton.ts
+++ src/13-singleton.ts
@@ -1,17 +1,27 @@
 // Avoid to create multiple instances of the same class.
 
 export class MyService {
-  constructor(private _name: string) {}
+  static instance: MyService | null = null;
+
+  private constructor(private _name: string) {}
 
   get name() {
     return this._name;
   }
+
+  static create(name: string) {
+    if (MyService.instance === null) {
+      console.log('runs only the first time');
+      MyService.instance = new MyService(name);
+    }
+    return MyService.instance;
+  }
 }
 
-const myService1 = new MyService('Service 1');
+const myService1 = MyService.create('Service 1');
 console.log('myService1', myService1.name);
 
-const myService2 = new MyService('Service 2');
+const myService2 = MyService.create('Service 2');
 console.log('myService2', myService2.name);
 
 console.log('same instance? ', myService1 === myService2);
